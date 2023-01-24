a2031c6755c58cbc5c76316212b8bde6a99636e2
NewbieTop <71062404+qkrfowns45@users.noreply.github.com>
2023-01-24 16:04:28 +0900

    security 기본설정

---
+++ photogram/src/main/java/com/newbie/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.newbie.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity //해당 파일로 security활성화
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//super 삭제 - 기존 시큐리티가 가지고 있는 기능이 다 비활겅화됨
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/","/user/**","/image/**","/subscribe/**","/comment/**").authenticated()
+			.anyRequest().permitAll()
+			.and()
+			.formLogin()
+			.loginPage("/auth/signin")
+			.defaultSuccessUrl("/");
+	}
+}
