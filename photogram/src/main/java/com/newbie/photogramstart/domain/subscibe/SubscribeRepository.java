9354c9a91eb8ce48cdf581a66f285f883533176d
NewbieTop <71062404+qkrfowns45@users.noreply.github.com>
2023-02-02 19:54:37 +0900

    구독하기 기능 테이블 만들기 위한 모델 만들기

---
+++ photogram/src/main/java/com/newbie/photogramstart/domain/subscibe/SubscribeRepository.java
@@ -0,0 +1,7 @@
+package com.newbie.photogramstart.domain.subscibe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscribeRepository extends JpaRepository<Subscribe, Integer>{
+
+}
