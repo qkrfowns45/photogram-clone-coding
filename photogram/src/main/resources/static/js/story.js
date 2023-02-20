7e52fc3f58ab0383e4e69b48d144c872370ace8e
NewbieTop <71062404+qkrfowns45@users.noreply.github.com>
2023-02-20 20:04:06 +0900

    페이징하기 및 스크롤 구현해서 로딩 구현

--- photogram/src/main/resources/static/js/story.js
+++ photogram/src/main/resources/static/js/story.js
@@ -8,13 +8,15 @@
  */
 
 // (1) 스토리 로드하기
+let page = 0;
+
 function storyLoad() {
 	$.ajax({
-		url:`/api/image`,
+		url:`/api/image?page = ${page}`,
 		dataType:"json"
 	}).done(res=>{
 		console.log(res);
-		res.data.forEach((image)=>{
+		res.data.content.forEach((image)=>{
 			let stroyItem = getStoryItem(image);
 			$("#storyList").append(stroyItem);
 		});
@@ -79,7 +81,12 @@ return item;
 
 // (2) 스토리 스크롤 페이징하기
 $(window).scroll(() => {
-
+	let checkNum = $(window).scrollTop() - ($(document).height() - $(window).height());
+	
+	if(checkNum < 1 && checkNum > -1){
+		page++;
+		storyLoad();
+	}
 });
 
 
