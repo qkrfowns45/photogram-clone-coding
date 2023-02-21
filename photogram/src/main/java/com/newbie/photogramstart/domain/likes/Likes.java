dc0bfe9a99dc69ed7a2bd86c683f6d8eee346002
NewbieTop <71062404+qkrfowns45@users.noreply.github.com>
2023-02-21 20:26:43 +0900

    좋아요 서비스 구현하기 위해 모델 및 좋아요 및 취소 API 생성

---
+++ photogram/src/main/java/com/newbie/photogramstart/domain/likes/Likes.java
@@ -0,0 +1,59 @@
+package com.newbie.photogramstart.domain.likes;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.newbie.photogramstart.domain.image.Image;
+import com.newbie.photogramstart.domain.subscibe.Subscribe;
+import com.newbie.photogramstart.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(
+		uniqueConstraints = {
+				@UniqueConstraint(
+						name = "likes_uk",
+						columnNames = {"imageId","userId"}
+				)
+		}
+)
+public class Likes { //N
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //번호증가 전략이 데이터베이스를 따라간다.
+	private int id;
+	
+	@JoinColumn(name = "imageId")
+	@ManyToOne
+	private Image image;//1
+	
+	// 오류가 터지고 나서 잡아보자
+	@JoinColumn(name = "userId")
+	@ManyToOne
+	private	User user; //1
+	
+	private LocalDateTime createDate;
+	
+	@PrePersist //디비에 INSERT 되기 직전에 실행
+	public void createDate() {
+		this.createDate = LocalDateTime.now();
+	}
+
+}
